453dd021a03e70409960757b5feb96fa6dea4cc4
AxelGzCa <2005045@utrivieramaya.edu.mx>
2022-02-16 20:19:24 -0500

    Update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+<div class="about">
+  <h1>This is an about page</h1>
+  <div>
+    <div  :key="index" v-for="(p,index) in result">
+    <h3>{{p.title}}</h3>
+    <img :src="p.thumbnail " alt="">
+  </div>
+</div>
   </div>
 </template>
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    result: null,
+  }),
+  created() {
+    axios.get("https://api.mercadolibre.com/sites/MLA/search?q=Motorola").then((result)=>{
+      this.result=result.data.results;
+    });
+  }
+}
+</script>
